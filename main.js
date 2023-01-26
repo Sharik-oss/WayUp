052f95e8675bd7ab059eab9da9fd00c4aa46a2f2
Sharik-oss <62305699+Sharik-oss@users.noreply.github.com>
2023-01-26 22:00:27 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var btn = document.getElementById('btn');
+
+btn.onclick = function (e) {
+	e.preventDefault();
+	var text = document.querySelector('p.intro');
+	text.classList.add('red');
+	var img = document.querySelector('.travel');
+	img.style.display = 'none';
+	document.querySelector('.future-value').style.marginLeft = "50px";
+}
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.future-value').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+})
